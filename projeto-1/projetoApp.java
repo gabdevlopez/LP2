f19dd6c31e8076b1649ec73191a0dce6ccaf1fd4
Gabriel <gabriel.algusto2010@gmail.com>
2022-04-01 19:13:49 -0300

    att

--- projeto-1/projetoApp.java
+++ projeto-1/projetoApp.java
@@ -22,7 +22,7 @@ class ListFrame extends JFrame {
     Point posMouse = null;
     Figure focus;
 
-    int rgb2[];
+    int rgb[];
     int vermelho[] = new int[] {255, 0, 0};
     int preto[] = new int[] {0, 0, 0};
     int branco[] = new int[]{255,255,255};
@@ -94,19 +94,19 @@ class ListFrame extends JFrame {
                         y = A.nextInt(350);
                     }
 
-                    rgb2 = new int[] {A.nextInt(255), A.nextInt(255), A.nextInt(255)};
+                    rgb = new int[] {A.nextInt(255), A.nextInt(255), A.nextInt(255)};
 
                     if (evt.getKeyChar() == 'r' || evt.getKeyChar() == 'R'){
-                        figs.add(new Rect(x,y, preto, rgb2));
+                        figs.add(new Rect(x,y, preto, rgb));
                     } 
                     if (evt.getKeyChar() == 'e' || evt.getKeyChar() == 'E'){
-                        figs.add(new Elipse(x,y, preto, rgb2));
+                        figs.add(new Elipse(x,y, preto, rgb));
                     } 
                     if (evt.getKeyChar() == 'p' || evt.getKeyChar() == 'P'){
-                        figs.add(new poligono(x,y, preto, rgb2));
+                        figs.add(new poligono(x,y, preto, rgb));
                     }
                     if (evt.getKeyChar() == 't' || evt.getKeyChar() == 'T'){
-                        figs.add(new Texto("ola galera!", x,y, branco));
+                        figs.add(new Texto("olá galera!", x,y, branco));
                     } 
 
                     if (focus != null){
---
+++ projeto-1/readme.md
@@ -0,0 +1,33 @@
+<h1>Comandos do programa</h1>
+
+<h2>Teclas:</h2>
+
+<h3>Criando as figuras</h3>
+
+<ul>    
+    <li>Ao apertar a tecla <b>E</b> surge uma elipse</li>
+    <li>Ao apertar a tecla <b>R</b> surge um retângulo</li>
+    <li>Ao apertar a tecla <b>T</b> surge um texto com o conteúdo: "Olá galera!"</li>
+    <li>Ao apertar a tecla <b>P</b> surge um polígono de 5 lados</li>
+</ul>
+
+<h3>Criando as figuras</h3>
+
+<p>Parad deletar alguma forma, basta clicar na tecla "<b>delete</b>"</p>
+
+<br>
+
+<p>As figuras surgem onde o mouse estiver, mas caso o mouse esteja de fora da janela elas surgirão de forma aleatória</p>
+
+<h3>Movendo as figuras</h3>
+
+<p>Para mover uma figura é só clicar sobre ela, segurar o botão direito do mouse e arrastar ou, se preferir, utilize as setas do teclado</p>
+
+<h3>Redimensionando</h3>
+
+<p>Clique na figura que deseja redimensionar e em seguida:</p>
+<ul>    
+    <li>Ao apertar a tecla "<b>-</b>" diminui o tamanho da figura</li>
+    <li>Ao apertar a tecla "<b>+</b>" (<i>ou a tecla "=" para notebooks sem a parte numérica do teclado</i>) aumenta o tamanho da figura</li>
+</ul>
+
